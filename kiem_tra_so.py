f1d08b7c33afd7774f382f76fdf09e300b6b11b6
phamhieu6307 <hieu1803992475@gmail.com>
2025-11-17 10:04:29 +0700

    Create kiem_tra_so.py

---
+++ kiem_tra_so.py
@@ -0,0 +1,5 @@
+n = int(input("Nhập một số: "))
+if n % 2 == 0 and n > 10 and n % 3 == 0:
+    print(" số này thỏa mãn 3 điều kiện ")
+else:
+    print(" số này không thỏa mãn 3 điều kiện ")         
